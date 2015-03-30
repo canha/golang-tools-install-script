9bc415845344dfa922f1df83b734e5648c6e0f46
Jorge Canha <jcanha.dev@gmail.com>
2015-03-30 17:46:45 +0100

    A little code cleanup, bump to v1.4.2

--- main.sh
+++ main.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-VERSION="1.4.1"
+VERSION="1.4.2"
 
 if [ "$1" == "--32" ]; then
     DFILE="go$VERSION.linux-386.tar.gz"
 elif [ "$1" == "--64" ]; then
     DFILE="go$VERSION.linux-amd64.tar.gz"
 elif [ "$1" == "--remove" ]; then
-    rm -rf $HOME/.go/
-    rm -rf $HOME/go/
-    sed -i '/# GoLang/d' $HOME/.profile
-    sed -i '/export GOROOT/d' $HOME/.profile
-    sed -i '/:$GOROOT/d' $HOME/.profile
-    sed -i '/export GOPATH/d' $HOME/.profile
-    sed -i '/:$GOPATH/d' $HOME/.profile
+    rm -rf "$HOME/.go/"
+    rm -rf "$HOME/go/"
+    sed -i '/# GoLang/d' "$HOME/.profile"
+    sed -i '/export GOROOT/d' "$HOME/.profile"
+    sed -i '/:$GOROOT/d' "$HOME/.profile"
+    sed -i '/export GOPATH/d' "$HOME/.profile"
+    sed -i '/:$GOPATH/d' "$HOME/.profile"
     echo "Go removed!"
     exit 0
 else
@@ -20,7 +20,7 @@ else
     exit 1
 fi
 
-if [ -d $HOME/.go ] || [ -d $HOME/go ]; then
+if [ -d "$HOME/.go" ] || [ -d "$HOME/go" ]; then
     echo "Installation directories already exist. Exiting."
     exit 1
 fi
@@ -31,16 +31,17 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-tar -C $HOME -xzf /tmp/go.tar.gz
-mv $HOME/go $HOME/.go
-touch $HOME/.profile
-echo '# GoLang' >> $HOME/.profile
-echo 'export GOROOT=$HOME/.go' >> $HOME/.profile
-echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.profile
-mkdir -p $HOME/go/src
-mkdir -p $HOME/go/pkg
-mkdir -p $HOME/go/bin
-echo 'export GOPATH=$HOME/go' >> $HOME/.profile
-echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.profile
-source $HOME/.profile
+tar -C "$HOME" -xzf /tmp/go.tar.gz
+mv "$HOME/go" "$HOME/.go"
+touch "$HOME/.profile"
+{
+    echo '# GoLang'
+    echo 'export GOROOT=$HOME/.go'
+    echo 'export PATH=$PATH:$GOROOT/bin'
+    echo 'export GOPATH=$HOME/go'
+    echo 'export PATH=$PATH:$GOPATH/bin'
+} >> "$HOME/.profile"
+
+mkdir -p "$HOME/go/{src,pkg,bin}"
+source "$HOME/.profile"
 rm -f /tmp/go.tar.gz
