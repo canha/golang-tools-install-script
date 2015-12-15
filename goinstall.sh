574f70b4acbd81aa71e9006226321543e6cdd202
Jorge Canha <jcanha.dev@gmail.com>
2015-12-15 16:57:58 +0000

    Fix #5

--- goinstall.sh
+++ goinstall.sh
@@ -55,7 +55,7 @@ touch "$HOME/.bashrc"
     echo 'export PATH=$PATH:$GOPATH/bin'
 } >> "$HOME/.bashrc"
 
-mkdir -p "$HOME/go/{src,pkg,bin}"
+mkdir -p $HOME/go/{src,pkg,bin}
 echo -e "\nGo $VERSION was installed.\nMake sure to relogin into your shell or run:"
 echo -e "\n\tsource $HOME/.bashrc\n\nto update your environment variables."
 echo "Tip: Opening a new terminal window usually just works. :)"
