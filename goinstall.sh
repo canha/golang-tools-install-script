099c885bb94c500511ce7e93aee4b054cf198685
Vadym Haidamaka <vadym.haidamaka@gmail.com>
2018-08-28 22:58:07 +0300

    Check environment variables GOROOT and GOPATH before installation

--- goinstall.sh
+++ goinstall.sh
@@ -3,6 +3,9 @@ set -e
 
 VERSION="1.9.2"
 
+[ -z "$GOROOT" ] && GOROOT="$HOME/.go"
+[ -z "$GOPATH" ] && GOPATH="$HOME/go"
+
 print_help() {
     echo "Usage: bash goinstall.sh OPTIONS"
     echo -e "\nOPTIONS:"
@@ -30,7 +33,7 @@ elif [ "$1" == "--arm" ]; then
 elif [ "$1" == "--darwin" ]; then
     DFILE="go$VERSION.darwin-amd64.tar.gz"
 elif [ "$1" == "--remove" ]; then
-    rm -rf "$HOME/.go/"
+    rm -rf "$GOROOT"
     sed -i '/# GoLang/d' "$HOME/.${shell_profile}"
     sed -i '/export GOROOT/d' "$HOME/.${shell_profile}"
     sed -i '/:$GOROOT/d' "$HOME/.${shell_profile}"
@@ -46,8 +49,8 @@ else
     exit 1
 fi
 
-if [ -d "$HOME/.go" ] || [ -d "$HOME/go" ]; then
-    echo "The 'go' or '.go' directories already exist. Exiting."
+if [ -d "$GOROOT" ] || [ -d "$GOPATH" ]; then
+    echo "The "$GOROOT" or "$GOPATH" directories already exist. Exiting."
     exit 1
 fi
 
@@ -60,18 +63,18 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Extracting File..."
-tar -C "$HOME" -xzf /tmp/go.tar.gz
-mv "$HOME/go" "$HOME/.go"
+tar -C /tmp -xzf /tmp/go.tar.gz
+mv /tmp/go "$GOROOT"
 touch "$HOME/.${shell_profile}"
 {
     echo '# GoLang'
-    echo 'export GOROOT=$HOME/.go'
+    echo "export GOROOT=${GOROOT}"
     echo 'export PATH=$PATH:$GOROOT/bin'
-    echo 'export GOPATH=$HOME/go'
+    echo "export GOPATH=$GOPATH"
     echo 'export PATH=$PATH:$GOPATH/bin'
 } >> "$HOME/.${shell_profile}"
 
-mkdir -p $HOME/go/{src,pkg,bin}
+mkdir -p $GOPATH/{src,pkg,bin}
 echo -e "\nGo $VERSION was installed.\nMake sure to relogin into your shell or run:"
 echo -e "\n\tsource $HOME/.${shell_profile}\n\nto update your environment variables."
 echo "Tip: Opening a new terminal window usually just works. :)"
