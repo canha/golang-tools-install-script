bad3c3408eed5984534f32c43d5ffd5d03c0584f
Jorge Canha <jcanha.dev@gmail.com>
2019-06-06 23:09:53 +0100

    Let the script detect the OS and CPU architecture by itself

--- goinstall.sh
+++ goinstall.sh
@@ -6,47 +6,68 @@ VERSION="1.12.5"
 [ -z "$GOROOT" ] && GOROOT="$HOME/.go"
 [ -z "$GOPATH" ] && GOPATH="$HOME/go"
 
+OS="$(uname -s)"
+ARCH="$(uname -m)"
+
+case $OS in
+    "Linux")
+        case $ARCH in
+        "x86_64")
+            ARCH=amd64
+            ;;
+        "armv6")
+            ARCH=armv6l
+            ;;
+        "armv8")
+            ARCH=arm64
+            ;;
+        .*386.*)
+            ARCH=386
+            ;;
+        esac
+        platform="linux-$ARCH"
+    ;;
+    "Darwin")
+        platform="darwin-amd64"
+    ;;
+esac
+
 print_help() {
     echo "Usage: bash goinstall.sh OPTIONS"
     echo -e "\nOPTIONS:"
-    echo -e "  --32\t\tInstall 32-bit version"
-    echo -e "  --64\t\tInstall 64-bit version"
-    echo -e "  --arm\t\tInstall armv6 version"
-    echo -e "  --darwin\tInstall darwin version"
-    echo -e "  --remove\tTo remove currently installed version"
+    echo -e "  --remove\tRemove currently installed version"
 }
 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-    # assume Zsh
     shell_profile="zshrc"
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-    # assume Bash
     shell_profile="bashrc"
 fi
 
-if [ "$1" == "--32" ]; then
-    DFILE="go$VERSION.linux-386.tar.gz"
-elif [ "$1" == "--64" ]; then
-    DFILE="go$VERSION.linux-amd64.tar.gz"
-elif [ "$1" == "--arm" ]; then
-    DFILE="go$VERSION.linux-armv6l.tar.gz"
-elif [ "$1" == "--darwin" ]; then
-    DFILE="go$VERSION.darwin-amd64.tar.gz"
-elif [ "$1" == "--remove" ]; then
+PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
+
+if [ "$1" == "--remove" ]; then
     rm -rf "$GOROOT"
-    sed -i "" '/# GoLang/d' "$HOME/.${shell_profile}"
-    sed -i "" '/export GOROOT/d' "$HOME/.${shell_profile}"
-    sed -i "" '/:$GOROOT/d' "$HOME/.${shell_profile}"
-    sed -i "" '/export GOPATH/d' "$HOME/.${shell_profile}"
-    sed -i "" '/:$GOPATH/d' "$HOME/.${shell_profile}"
+    if [ "$OS" == "Darwin" ]; then
+        sed -i "" '/# GoLang/d' "$HOME/.${shell_profile}"
+        sed -i "" '/export GOROOT/d' "$HOME/.${shell_profile}"
+        sed -i "" '/:$GOROOT/d' "$HOME/.${shell_profile}"
+        sed -i "" '/export GOPATH/d' "$HOME/.${shell_profile}"
+        sed -i "" '/:$GOPATH/d' "$HOME/.${shell_profile}"
+    else
+        sed -i '/# GoLang/d' "$HOME/.${shell_profile}"
+        sed -i '/export GOROOT/d' "$HOME/.${shell_profile}"
+        sed -i '/:$GOROOT/d' "$HOME/.${shell_profile}"
+        sed -i '/export GOPATH/d' "$HOME/.${shell_profile}"
+        sed -i '/:$GOPATH/d' "$HOME/.${shell_profile}"
+    fi
     echo "Go removed."
     exit 0
 elif [ "$1" == "--help" ]; then
     print_help
     exit 0
 else
-    print_help
-    exit 1
+    echo "Unrecognized option: $1"
 fi
 
 if [ -d "$HOME/.go" ]; then
@@ -54,11 +75,11 @@ if [ -d "$HOME/.go" ]; then
     exit 1
 fi
 
-echo "Downloading $DFILE ..."
+echo "Downloading $PACKAGE_NAME ..."
 if hash wget 2>/dev/null; then
-    wget https://storage.googleapis.com/golang/$DFILE -O /tmp/go.tar.gz
+    wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O /tmp/go.tar.gz
 else
-    curl -o /tmp/go.tar.gz https://storage.googleapis.com/golang/$DFILE
+    curl -o /tmp/go.tar.gz https://storage.googleapis.com/golang/$PACKAGE_NAME
 fi
 
 if [ $? -ne 0 ]; then
