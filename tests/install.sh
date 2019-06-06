4ed436428f8fe9cd45ee6368a084b6ced072767a
Jorge Canha <jcanha.dev@gmail.com>
2019-06-07 00:16:02 +0100

    Add install tests and Travis

---
+++ tests/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+echo "Go environment before setup:"
+env | grep "^GO"
+
+"$(dirname "$0")/../goinstall.sh"
+
+source ~/.bashrc
+
+echo "Go environment after setup:"
+env | grep "^GO"
+cat ~/.bashrc
+cat ~/.profile
+
+mkdir -p "$GOPATH/src/hello"
+pushd "$GOPATH/src/hello"
+cat >hello.go <<EOF
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+func main() {
+    fmt.Printf("go version %s %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+}
+EOF
+go build
+go install
+hello
+popd
