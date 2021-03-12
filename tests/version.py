2009b061824e2116ee03563d62cbbc4b41925ff6
HZH <42015599+capric98@users.noreply.github.com>
2021-03-12 23:14:38 +0800

    lightweight response

--- tests/version.py
+++ tests/version.py
@@ -3,5 +3,5 @@
 import requests
 import re
 
-resp = requests.get("https://golang.org/dl").text
+resp = requests.get("https://golang.org/dl/?mode=json").text
 print(re.findall("(?<=go)[.,0-9]+(?=.src.tar.gz)", resp)[0], end="")
